8de1c51094f865b26a43cde0d6db9c57867fc233
Teepan <paartheepan94@gmail.com>
2022-08-16 17:04:44 +0200

    chore: add offline access to api responses

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from '@honkhonk/vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()]
+  plugins: [
+    svgr(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg}',
+        ],
+        runtimeCaching: [{
+          urlPattern: new RegExp('^https://travel-troupe-api.herokuapp.com/'),
+          handler: 'NetworkFirst',
+            options: {
+            networkTimeoutSeconds: 20,
+            cacheName: 'api-cache',
+            cacheableResponse: {
+              statuses: [0, 200],
+            },
+          },
+        }]
+      },
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
 })
