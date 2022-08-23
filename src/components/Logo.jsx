f824c966e4784645aa9ad8441246a2a8575cdd66
Naouel Zenati <zenati.naouel14@gmail.com>
2022-08-24 00:07:31 +0200

    Fix : form css

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const Logo = styled.img`
 display: inline-block;
 width: 100px;
+margin-bottom: 4rem;
   /* background: ${({secondary}) => secondary ? 'var(---color-green);' : 'blue'}; */
 `
 
