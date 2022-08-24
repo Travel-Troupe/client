53c3f16c1dd15ae9b18388a9816efd62b7291dc8
Adrien <adrien.bannwarth@colorz.fr>
2022-08-24 09:37:51 +0200

    add text settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,16 +1,31 @@
 import React, { useContext } from 'react'
+import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
 import Button from '../components/Button'
 import { logout } from '../services/User'
 import AuthContext from '../store/contexts/AuthContext'
+import AppHeader from '../components/AppHeader'
 
 const Settings = () => {
   const { dispatch } = useContext(AuthContext)
   const navigateTo = useNavigate();
 
+  const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  button {
+    margin-top: 50%;
+    width: fit-content;
+  }
+`
 
   return (
-    <Button onClick={() => {logout({ dispatch }); window.location.href = '/'}}>Se déconnecter</Button>
+    <StyledContent>
+      <AppHeader title="Paramètres" subtitle="Se déconnecter"></AppHeader>
+      <Button onClick={() => {logout({ dispatch }); window.location.href = '/'}}>Se déconnecter</Button>
+    </StyledContent>
   )
 }
 
